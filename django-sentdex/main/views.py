f802c95657aec44c250f9a4f109d160ac7db0a50
Roderich25 <rodrigoavilasolis@gmail.com>
2019-09-12 20:40:33 -0500

    adding tutorial template

--- django-sentdex/main/views.py
+++ django-sentdex/main/views.py
@@ -12,14 +12,21 @@ def single_slug(request, single_slug):
         matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
         series_urls = {}
         for m in matching_series.all():
-            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest(
+                "tutorial_published")
             series_urls[m] = part_one.tutorial_slug
         return render(request,
                       "main/category.html",
                       {"part_ones": series_urls})
     tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
     if single_slug in tutorials:
-        return HttpResponse(f"{single_slug} is a tutorial.")
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+        tutorials_from_series = Tutorial.objects.filter(tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by("tutorial_published")
+
+
+        return render(request,
+                      "main/tutorial.html",
+                      {"tutorial": this_tutorial})
     return HttpResponse(f"{single_slug} does not exist!")
 
 
