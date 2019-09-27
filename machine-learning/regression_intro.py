9ae1119acda36024c31f39f100d7dd8c6e00b5c7
Roderich25 <rodrigoavilasolis@gmail.com>
2019-09-27 12:10:16 -0500

    data split into train and test sets

--- machine-learning/regression_intro.py
+++ machine-learning/regression_intro.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import quandl
-import math
-import os
+import quandl, math, os
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 quandl.ApiConfig.api_key = os.getenv('QUANDL_API_KEY')
 df = quandl.get('WIKI/GOOGL')
@@ -13,7 +15,16 @@ df = df[['Adj. Close', 'HL_Pct', 'Pct_change', 'Adj. Volume']]
 
 output = 'Adj. Close'
 df.fillna(-99999, inplace=True)
-#test_n = int(math.ceil(0.1*len(df)))
-df['label'] = df[output].shift(-2)
+df['label'] = df[output].shift(-10)
 df.dropna(inplace=True)
-print(df.head(25))
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
