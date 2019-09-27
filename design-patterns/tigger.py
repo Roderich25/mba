c3d3699812bed24f9617b7a38456eae6e3544de3
Roderich25 <rodrigoavilasolis@gmail.com>
2019-09-27 16:11:17 -0500

    adding design-patterns project and singlenton code for untrained eye

---
+++ design-patterns/tigger.py
@@ -0,0 +1,17 @@
+class _Tigger:  # leading underscore means private class
+
+    def __str__(self):
+        return "I'm the only one"
+
+    def roar(self):
+        return "Grrr!"
+
+
+_instance = None
+
+
+def Tigger():
+    global _instance
+    if _instance is None:
+        _instance = _Tigger()
+    return _instance
