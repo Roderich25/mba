3c99718acfbbb4382c651f353907306492c52cfe
Roderich25 <rodrigoavilasolis@gmail.com>
2019-09-27 19:41:23 -0500

    multiprocessing process

--- python-threading/multiprocessing_demo.py
+++ python-threading/multiprocessing_demo.py
@@ -1,17 +1,23 @@
+import multiprocessing
 import time
 
 start = time.perf_counter()
 
 
-def do_something():
-    print('Sleeping...')
-    time.sleep(1)
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
     print('Sleeping done.')
 
 
-do_something()
-do_something()
+processes = []
+for _ in range(10):
+    p = multiprocessing.Process(target=do_something, args=[1.5])
+    p.start()
+    processes.append(p)
+
+for process in processes:
+    process.join()
 
 finish = time.perf_counter()
-
 print(f"Finished in {round(finish - start, 2)} second(s)")
