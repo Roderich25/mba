3ce7074f7d0cec0175f0fcb6d7f9c935dd023edd
Roderich25 <rodrigoavilasolis@gmail.com>
2019-09-28 09:31:51 -0500

    ML forecasting and predicting

--- machine-learning/regression_intro.py
+++ machine-learning/regression_intro.py
@@ -1,9 +1,13 @@
 import pandas as pd
-import quandl, math, os
+import quandl, math, os, datetime
 import numpy as np
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 quandl.ApiConfig.api_key = os.getenv('QUANDL_API_KEY')
 df = quandl.get('WIKI/GOOGL')
@@ -13,18 +17,45 @@ df['HL_Pct'] = (df['Adj. High']-df['Adj. Close'])/df['Adj. Close'] * 100
 df['Pct_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
 df = df[['Adj. Close', 'HL_Pct', 'Pct_change', 'Adj. Volume']]
 
+
 output = 'Adj. Close'
 df.fillna(-99999, inplace=True)
-df['label'] = df[output].shift(-10)
-df.dropna(inplace=True)
+forecast_out = 10
+df['label'] = df[output].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_last = X[-forecast_out:]
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-clf = LinearRegression()
+
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+
+
+forecast_set = clf.predict(X_last)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86_400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
