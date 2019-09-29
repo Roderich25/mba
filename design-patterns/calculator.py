bfe2c04ac9ecdbc82665af112b5c024d2b4d0e31
Roderich25 <rodrigoavilasolis@gmail.com>
2019-09-28 19:54:20 -0500

    template method behairvoral desgin pattern

--- design-patterns/calculator.py
+++ design-patterns/calculator.py
@@ -2,4 +2,59 @@ from abc import ABC, abstractmethod
 
 
 class AverageCalculator(ABC):
-    pass
+
+    def average(self):
+        try:
+            num_items = 0
+            total_sum = 0
+            while self.has_next():
+                total_sum += self.next_item()
+                num_items += 1
+            if num_items == 0:
+                raise RuntimeError("Can't compute average of zero items.")
+            return total_sum/num_items
+        finally:
+            self.dispose()
+
+    @abstractmethod
+    def has_next(self):
+        pass
+
+    @abstractmethod
+    def next_item(self):
+        pass
+
+    def dispose(self):
+        pass
+
+class FileAverageCalculator(AverageCalculator):
+
+    def __init__(self, file):
+        self.file = file
+        self.last_line = self.file.readline()
+
+    def has_next(self):
+        return self.last_line != ''
+
+    def next_item(self):
+        result = float(self.last_line)
+        self.last_line = self.file.readline()
+        return result
+
+    def dispose(self):
+        self.file.close()
+
+
+class MemoryAverageCalculator(AverageCalculator):
+
+    def __init__(self, list_in):
+        self.list_in = list_in
+        self.i = 0
+
+    def has_next(self):
+        return self.i < len(self.list_in)
+
+    def next_item(self):
+        output = self.list_in[self.i]
+        self.i += 1
+        return output
