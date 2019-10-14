10d4950bbda59fdd0aa1e28437b7ac8a30d4f8ad
Roderich25 <rodrigoavilasolis@gmail.com>
2019-10-14 15:20:24 -0500

    Flask CORS example

---
+++ flask-cors-demo/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+# CORS(app)
+
+
+@app.route('/api')
+@cross_origin()
+def api():
+    return jsonify({'data': 'Hello World!'})
+
+
+if __name__ == '__main__':
+    app.run()
