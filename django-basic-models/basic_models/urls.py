69d7d643266f68cb84cd2067cdb3973f6644f6b0
Roderich25 <rodrigoavilasolis@gmail.com>
2019-10-17 13:05:10 -0500

    django models changes

--- django-basic-models/basic_models/urls.py
+++ django-basic-models/basic_models/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "My Admin"
+admin.site.site_title = "My Admin Portal"
+admin.site.index_title = "Welcome to the Admin portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
--- django-basic-models/example/models.py
+++ django-basic-models/example/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=40)
+    date_created = models.DateField(null=True)
 
     def __str__(self):
         return f'{self.name} Inc.'
@@ -20,6 +21,7 @@ class Language(models.Model):
 
 class Programmer(models.Model):
     name = models.CharField(max_length=60)
+    birth_year = models.PositiveSmallIntegerField(default=0)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     languages = models.ManyToManyField(Language)
 
