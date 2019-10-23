614492eba6fa9ff18dbe580b6b3213feaeb5364f
Roderich25 <rodrigoavilasolis@gmail.com>
2019-10-23 10:18:02 -0500

    class-based views app added to django by example

--- django-by-example/class_based_views/views.py
+++ django-by-example/class_based_views/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import View
+from django.http import HttpResponse
+from django.views.generic import View, TemplateView, ListView
+from blog.models import Post
 
 
 class MyView(View):
-    pass
+
+    def get(self, request):
+        return HttpResponse("Hello, World!")
+
+
+class MyTemplateView(TemplateView):
+    template_name = 'class_based_views/template.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = [p.titulo for p in Post.publicados.all()]
+        return context
+
+
+class MyListView(ListView):
+    model = Post
+    paginate_by = 4
+    template_name = 'class_based_views/list.html'
+
+
--- django-by-example/mysite/settings.py
+++ django-by-example/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'class_based_views.apps.ClassBasedViewsConfig'
 ]
 
 MIDDLEWARE = [
