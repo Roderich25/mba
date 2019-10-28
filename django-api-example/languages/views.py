b5d360810de7713dcae91ce9861c9229e915b668
Roderich25 <rodrigoavilasolis@gmail.com>
2019-10-28 15:38:48 -0600

    rest framework permissions

--- django-api-example/languages/views.py
+++ django-api-example/languages/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from rest_framework import viewsets, permissions
+from .models import Language, Paradigm, Programmer
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
 
 
 class LanguageView(viewsets.ModelViewSet):
     serializer_class = LanguageSerializer
     queryset = Language.objects.all()
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class ParadigmView(viewsets.ModelViewSet):
+    serializer_class = ParadigmSerializer
+    queryset = Paradigm.objects.all()
+
+
+class ProgrammerView(viewsets.ModelViewSet):
+    serializer_class = ProgrammerSerializer
+    queryset = Programmer.objects.all()
