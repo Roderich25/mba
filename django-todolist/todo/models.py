f1b90dd6a28bd9fc8b9ede36ca5d4e1ac389a218
Roderich25 <rodrigoavilasolis@gmail.com>
2019-10-29 12:02:44 -0600

    customizing django admin page

--- django-todolist/todo/models.py
+++ django-todolist/todo/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Todo(models.Model):
-    text = models.CharField(max_length=40)
+    text = models.TextField()
     completed = models.BooleanField(default=False)
+    font_size = models.PositiveSmallIntegerField(null=True)
 
     def __str__(self):
         return self.text
+
+    def text_preview(self):
+        return self.text[:50]
