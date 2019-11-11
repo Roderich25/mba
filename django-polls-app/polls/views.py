25af7ca7ddc1da8d3d5d57bd005d17619dcefdcb
Roderich25 <rodrigoavilasolis@gmail.com>
2019-11-11 12:55:53 -0600

    finished django pollster app

--- django-polls-app/polls/views.py
+++ django-polls-app/polls/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
 from .models import Question, Choice
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from django.shortcuts import HttpResponseRedirect, reverse
+from django.contrib import messages
 
 
-class HomeView(TemplateView):
-    template_name = 'polls/home.html'
+class HomeView(ListView):
+    model = Question
+    template_name = 'polls/index.html'
+    ordering = '-pub_date'
 
+
+class QuestionView(DetailView):
+    model = Question
+    fields = ['choice_text']
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, q_id):
+    if request.method == 'POST':
+        if request.POST.get('choice', False):
+            c = Choice.objects.get(pk=request.POST['choice'])
+            c.votes += 1
+            c.save()
+            messages.success(request, "Vote added.")
+        else:
+            messages.warning(request, "You didn't select a choice.")
+    return HttpResponseRedirect(reverse('polls:results', args=(q_id,)))
