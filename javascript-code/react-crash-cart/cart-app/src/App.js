874c6f541016b52eed40a849ab287d616b22a366
Roderich25 <rodrigoavilasolis@gmail.com>
2019-11-10 18:24:17 -0600

    react-cart-app finished

--- javascript-code/react-crash-cart/cart-app/src/App.js
+++ javascript-code/react-crash-cart/cart-app/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 4 }
+    ]
+  };
+
+  constructor() {
+    super();
+    console.log("App-Constructor");
+    //Initialize state
+  }
+
+  componentDidMount() {
+    //Ajax Call and then setState
+    console.log("App-Mounted");
+  }
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  render() {
+    console.log("App-Rendered");
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters
+            .filter(c => c.value > 0)
+            .map(c => c.value)
+            .reduce((a, c) => a + c, 0)}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
