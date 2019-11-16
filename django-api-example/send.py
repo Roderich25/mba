15cb33c30f3eafb68a48d889d242c5361c9c0cec
Roderich25 <rodrigoavilasolis@gmail.com>
2019-11-15 20:33:56 -0600

    cleaning

--- django-api-example/send.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-headers = {}
-
-headers['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTcyMzEwMTcyLCJqdGkiOiIzNmY4YWQxYmY2ZTI0MTRjYmI1MGY0YTY1N2M0ZDcwOCIsInVzZXJfaWQiOjF9.g_gdxDQustQJi6v0UOXsd3_oO0etSN7XJRQ8_RidsSk'
-
-r = requests.get('http://127.0.0.1:8000/programmers/', headers=headers)
-print(r.text)
