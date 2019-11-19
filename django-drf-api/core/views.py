2ae43bd20624eb38bf71dffb0483f774f8601f58
Roderich25 <rodrigoavilasolis@gmail.com>
2019-11-18 19:08:38 -0600

    finished drf api app

--- django-drf-api/core/views.py
+++ django-drf-api/core/views.py
@@ -3,20 +3,45 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from core.serializers import PostSerializer
 from core.models import Post
+from rest_framework import generics
+from rest_framework import mixins
 
+# class TestView(APIView):
+#
+#     permission_classes = [IsAuthenticated]
+#
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, *rgs, **kargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
 
-class TestView(APIView):
-
-    permission_classes = [IsAuthenticated]
+class PostView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *rgs, **kargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PostCreateView(mixins.ListModelMixin, generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
