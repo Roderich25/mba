67838410e0942d97294e99e75dcd0f8c98ccddd9
Roderich25 <rodrigoavilasolis@gmail.com>
2019-11-19 20:14:47 -0600

    update bookmarks app

--- django-bookmarks/account/views.py
+++ django-bookmarks/account/views.py
@@ -45,3 +45,17 @@ def user_login(request):
         form = LoginForm()
         print("two")
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form':profile_form})
