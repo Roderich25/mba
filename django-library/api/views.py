e914257890e16f1216a4d83c6633602caa401a9e
Roderich25 <rodrigoavilasolis@gmail.com>
2019-11-29 13:44:42 -0600

    basic library api app

---
+++ django-library/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+from books.models import Book
+from api.serializers import BookSerializer
+
+
+class BookAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
--- django-library/test.js
+++
@@ -1,5 +0,0 @@
-var a = 1;
-
-function a(){
-    console.log(a);
-}
