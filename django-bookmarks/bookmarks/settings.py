a5c758bde1faba7e881448d5cdfb0b44484b2b66
Roderich25 <rodrigoavilasolis@gmail.com>
2019-12-24 16:49:04 -0600

    update

--- django-bookmarks/bookmarks/settings.py
+++ django-bookmarks/bookmarks/settings.py
@@ -127,3 +127,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = [os.getenv('G2DOM')]
 ABSOLUTE_URL_OVERRIDES = {'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])}
 
 THUMBNAIL_DEBUG = True
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
