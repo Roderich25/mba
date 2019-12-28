b77a8bfc03ef7a2f01077567cb9fd3b90ec18801
Roderich25 <rodrigoavilasolis@gmail.com>
2019-12-27 18:23:19 -0600

    bookmarks project finished

--- django-bookmarks/images/views.py
+++ django-bookmarks/images/views.py
@@ -8,6 +8,10 @@ from images.models import Image
 from bookmarks.common.decorators import ajax_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
 @login_required
@@ -31,7 +35,11 @@ def image_list(request):
 @login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    total_views = r.incr(f'image:{image.id}:views')
+    r.zincrby('image_ranking', image.id, 1)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images', 'image': image, 'total_views': total_views})
 
 
 @login_required
@@ -69,3 +77,12 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'ko'})
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'ranking', 'most_viewed': most_viewed})
