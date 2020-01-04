03688e0d6049035965608214d5f9a0712b067d11
Roderich25 <rodrigoavilasolis@gmail.com>
2020-01-03 18:40:55 -0600

    cart template

--- django-myshop/cart/cart.py
+++ django-myshop/cart/cart.py
@@ -12,7 +12,7 @@ class Cart(object):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            cart = self.session[settings.CART_SESION_ID] = {}
+            cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, update_quantity=False):
