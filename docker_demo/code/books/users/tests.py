4013c3718bb587df6b7a0c4cb5ce1afd315f48a5
Roderich25 <rodrigoavilasolis@gmail.com>
2020-01-07 20:01:57 -0600

    adding testing code

--- docker_demo/code/books/users/tests.py
+++ docker_demo/code/books/users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUSerTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='John Doe', email='john.doe@example.com', password="Password12345")
+        self.assertEqual(user.username, 'John Doe')
+        self.assertEqual(user.email, 'john.doe@example.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superman', email='superman@example.com', password='Admin12345')
