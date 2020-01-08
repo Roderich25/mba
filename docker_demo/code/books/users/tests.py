879f17fae225a477947064704b6457167c7a0d52
Roderich25 <rodrigoavilasolis@gmail.com>
2020-01-07 20:08:02 -0600

    starting Pages app

--- docker_demo/code/books/users/tests.py
+++ docker_demo/code/books/users/tests.py
@@ -17,4 +17,9 @@ class CustomUSerTests(TestCase):
     def test_create_superuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
-            username='superman', email='superman@example.com', password='Admin12345')
+            username='SuperMan', email='superman@example.com', password='Admin12345')
+        self.assertEqual(admin_user.username, 'SuperMan')
+        self.assertEqual(admin_user.email, 'superman@example.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
