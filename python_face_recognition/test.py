1db38902cfa992cecf9a783ce469483864605dad
Roderich25 <rodrigoavilasolis@gmail.com>
2020-03-08 18:06:22 -0600

    update

--- python_face_recognition/test.py
+++ python_face_recognition/test.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import face_recognition
+import os
+import cv2
+
+KNOWN_FACES_DIR = 'known_faces'
+UNKNOWN_FACES_DIR = 'unknown_faces'
+TOLERANCE = 0.6
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+MODEL = "cnn"  # hog
+
+print("loading known faces ...")
+
+known_faces = []
