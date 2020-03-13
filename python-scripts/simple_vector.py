89912a46428e01d79143a7f0d6cdb9c9a62fdcfc
Roderich25 <rodrigoavilasolis@gmail.com>
2020-03-13 15:14:39 -0600

    adding simple vector from fluent python

---
+++ python-scripts/simple_vector.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from math import hypot
+
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Vector({self.x},{self.y})"
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x*scalar, self.y*scalar)
+
+
+v1 = Vector(3, 4)
+v2 = Vector(1, 2)
+print(v1+v2)
+print(abs(v1))
+print(v2*5)
+print(abs(v1+v2*6))
