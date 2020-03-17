44e0f462d9affba1d9885496e752f3b9f1ce5bec
Roderich25 <rodrigoavilasolis@gmail.com>
2020-03-17 05:33:44 -0600

    color change added for every point

--- python-scripts/points.py
+++ python-scripts/points.py
@@ -3,6 +3,7 @@ from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib import cm
 
 
 def set_direction():
@@ -15,12 +16,14 @@ def set_direction():
 
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, m, color):
         self.x = x
         self.y = y
         self.n = 0
+        self.m = m
+        self.color = color
         self.dir_x, self.dir_y = set_direction()
-        self.array = [[self.x], [self.y]]
+        self.array = [[self.x], [self.y], [self.color]]
 
     def move(self):
         self.x += self.dir_x
@@ -32,7 +35,10 @@ class Point:
             self.dir_y = self.dir_y * -1
         self.array[0].append(self.x)
         self.array[1].append(self.y)
-        print(self)
+        if self.n >= self.m:
+            self.array[2].append("blue")
+        else:
+            self.array[2].append(self.color)
         return self
 
     def move_n(self, n):
@@ -44,22 +50,38 @@ class Point:
         return f"Point({self.x},{self.y}) #{self.n}"
 
 
-a = Point(0, 0)
-sim = a.move_n(25).array
-
-
-def update_line(num, data, line):
-    line.set_data(data[..., num])
-    return (line,)
-
-
-fig = plt.figure()
+sim = []
+for i in enumerate(range(10)):
+    p = Point(randint(-5, 5), randint(-2, 2), i[0] * 3, (5 * ["red", "green"])[i[0]],)
+    s = p.move_n(30).array
+    sim.append(s)
 
 data = np.array(sim)
-(l,) = plt.plot([], [], ". r")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
 plt.xlim(-6, 6)
 plt.ylim(-3, 3)
-line_ani = animation.FuncAnimation(
-    fig, update_line, 25, fargs=(data, l), interval=500, blit=True
+plt.tick_params(
+    axis="both",
+    which="both",
+    bottom=False,
+    top=False,
+    labelbottom=False,
+    right=False,
+    left=False,
+    labelleft=False,
 )
+ims = []
+for i, num in enumerate(range(0, 30)):
+    x, y, c = [], [], []
+    for point in data[:, :3, num]:
+        x.append(int(point[0]))
+        y.append(int(point[1]))
+        c.append(point[2])
+    print(i, x, y, c)
+    scat = ax.scatter(x=x, y=y, s=50, c=c)
+    ims.append([scat])
+
+im_ani = animation.ArtistAnimation(fig, ims, interval=800, repeat_delay=300, blit=True)
 plt.show()
