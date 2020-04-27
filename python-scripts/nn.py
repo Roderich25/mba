1b65bc17f1187be5c17cb6f1563dcedb3ce71ce1
Roderich25 <rodrigoavilasolis@gmail.com>
2020-04-27 18:56:46 -0500

    update

--- python-scripts/nn.py
+++ python-scripts/nn.py
@@ -2,14 +2,33 @@ import sys
 import numpy as np
 import matplotlib
 
-print("Python:", sys.version)
-print("Numpy:", np.__version__)
-print("Matplotlib:", matplotlib.__version__)
+# print("Python:", sys.version)
+# print("Numpy:", np.__version__)
+# print("Matplotlib:", matplotlib.__version__)
 
+inputs = [1, 2, 3, 2.5]
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
+# weights1 = [0.2, 0.8, -0.5, 1.0]
+# weights2 = [0.5, -0.91, 0.26, -0.5]
+# weights3 = [-0.26, -0.27, 0.17, 0.87]
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
-print(output)
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+
+# bias1 = 2
+# bias2 = 3
+# bias3 = 0.5
+
+biases = [2, 3, 0.5]
+
+layer_outputs = []
+
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input * weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
+
+print(np.dot(weights, inputs) + biases)
