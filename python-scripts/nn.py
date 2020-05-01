1409ca4e50c750248c79cc3113537ddd06370176
Roderich25 <rodrigoavilasolis@gmail.com>
2020-05-01 14:00:27 -0500

    Layer class added

--- python-scripts/nn.py
+++ python-scripts/nn.py
@@ -2,25 +2,26 @@ import sys
 import numpy as np
 import matplotlib
 
+np.random.seed(0)
 # print("Python:", sys.version)
 # print("Numpy:", np.__version__)
 # print("Matplotlib:", matplotlib.__version__)
 
-inputs = np.array([[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]])
+X = np.array([[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]])
 
-weights = np.array(
-    [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-)
 
-biases = np.array([2, 3, 0.5])
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-# 2nd layer
-weights2 = np.array([[0.1, -0.14, -0.5], [-0.5, 0.12, -0.33], [-0.44, 0.73, -0.13]])
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-biases2 = np.array([-1, 2, -0.5])
 
-layer1_outputs = np.dot(inputs, weights.T) + biases
+layer1 = Layer_Dense(4, 50)
+layer2 = Layer_Dense(50, 2)
 
-layer2_outputs = np.dot(layer1_outputs, weights2.T) + biases2
-
-print(layer2_outputs)
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
