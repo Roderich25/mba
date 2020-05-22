661db911228a7da964587013cf581a0d412feebf
Roderich25 <rodrigoavilasolis@gmail.com>
2020-05-22 16:04:10 -0500

    SetenceIterator added

--- python-scripts/sentence.py
+++ python-scripts/sentence.py
@@ -11,26 +11,38 @@ class Sentence:
         self.text = text
         self.words = RE_WORD.findall(text)
 
-    def __getitem__(self, index):
-        return self.words[index]
-
-    def __len__(self):
-        return len(self.words)
-
     def __repr__(self):
         return f'Sentence({reprlib.repr(self.text)})'
+
+    def __iter__(self):
+        return SentenceIterador(self.words)
+
+
+class SentenceIterador:
+
+    def __init__(self, words):
+        self.words = words
+        self.index = 0
+
+    def __next__(self):
+        try:
+            word = self.words[self.index]
+        except IndexError:
+            raise StopIteration()
+        self.index += 1
+        return word
 
 
 s = Sentence("Alpha Bravo Charlie Delta Echo Foxtrot")
 print(s.words)
-print(s[-1])
-print(len(s))
 print(s)
+
 
 for w in s:
     print(w)
 
-print(list(s))
+# print(list(s))
 
-print(issubclass(Sentence, abc.Iterable))
+print(issubclass(SentenceIterador, abc.Iterator))
 print(isinstance(s, abc.Iterable))
+#
