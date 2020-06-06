4deb7c7c70b36d0a7b3c972b2a80bed61f7b3e6c
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-06 15:54:34 -0500

    django channels tutorial part 1 finished

--- django-channels-chat/chat/urls.py
+++ django-channels-chat/chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, RoomView
 
 
 app_name = 'chat_app'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<str:room_name>/', RoomView.as_view(), name='room'),
 ]
