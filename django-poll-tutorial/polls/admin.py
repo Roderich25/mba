dc38e37d45f7525b62c2de6b236ecce6e0718df8
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-09 16:00:50 -0500

    updating admin section

--- django-poll-tutorial/polls/admin.py
+++ django-poll-tutorial/polls/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Date of publication', {'fields': ['pub_date']}),
+        ('Question', {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
