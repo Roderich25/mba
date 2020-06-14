bf8ac04661c1236cea5164f1a5f64167bd171d0b
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-14 15:30:12 -0500

    update admin section

--- django-poll-tutorial/polls/admin.py
+++ django-poll-tutorial/polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Choice, Question
+from django.utils import timezone
 
 
 class ChoiceInLine(admin.TabularInline):
@@ -7,8 +8,16 @@ class ChoiceInLine(admin.TabularInline):
     extra = 1
 
 
+def created_now(modeladmin, requets, queryset):
+    queryset.update(pub_date=timezone.now())
+
+
+created_now.short_description = 'Mark as published now'
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    actions = [created_now]
     list_display = ['pub_date', 'question_text', 'was_published_recently', 'active']
     list_editable = ['question_text', 'active']
     list_filter = ['active', 'pub_date']
@@ -20,7 +29,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Question', {'fields': ['question_text']}),
     ]
     inlines = [ChoiceInLine]
-
 
 # admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Choice)
