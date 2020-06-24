d55d92ca44033289e6d00e5bc5d1af5db694e7bd
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-24 18:16:14 -0500

    adding requirements.txt

--- python-selenium/selenium_bs4.py
+++ python-selenium/selenium_bs4.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 import os
 import time
 
+# Selenium part
 chromeOptions = Options()
 chromeOptions.add_argument("--kiosk")
 chromeOptions.add_argument("--ignore-certificate-errors")
@@ -21,6 +22,13 @@ search_go.click()
 time.sleep(1)
 raul_jimenez = browser.find_element_by_partial_link_text("Raúl Jiménez")
 raul_jimenez.click()
+page_source = browser.page_source
 
-page_source = browser.page_source()
+# BeautifulSoup part
+soup = BeautifulSoup(page_source, 'lxml')
+title_finder = soup.find_all("span", class_="title")
+print(title_finder)
 
+print('*' * 10)
+for title in title_finder:
+    print(title.string)
