bbff05173871b82ad33eaae9fededf679d3a9e29
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-24 18:01:26 -0500

    update

---
+++ python-selenium/selenium_bs4.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+import os
+import time
+
+chromeOptions = Options()
+chromeOptions.add_argument("--kiosk")
+chromeOptions.add_argument("--ignore-certificate-errors")
+browser = webdriver.Chrome(os.getcwd() + '/chromedriver', options=chromeOptions)
+browser.get('https://www.premierleague.com')
+time.sleep(1)
+players = browser.find_element_by_link_text("Players")
+time.sleep(1)
+players.click()
+time.sleep(5)
+search = browser.find_element_by_id("search-input")
+search.send_keys("Raúl Jiménez")
+search_go = browser.find_element_by_class_name("searchIconContainer.searchCommit")
+search_go.click()
+time.sleep(1)
+raul_jimenez = browser.find_element_by_partial_link_text("Raúl Jiménez")
+raul_jimenez.click()
+
+page_source = browser.page_source()
+
