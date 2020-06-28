a2a266eae19aefd5bbb8c3dd2173727fc20c1dc3
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-27 21:32:01 -0500

    xpath changed

--- python-selenium/reforma.py
+++ python-selenium/reforma.py
@@ -4,8 +4,8 @@ import requests
 page = requests.get('http://www.reforma.com')
 tree = html.fromstring(page.content)
 
-news = tree.xpath('//div[@id="contenido"]//div[@id="unodostres"]//h1//a[@class="ligaonclick"]')
-ps = tree.xpath('//div[@id="contenido"]//div[@id="unodostres"]//p')
+news = tree.xpath('//div[@id="contenido"]//div[@id="zonaprime"]//h1//a[@class="ligaonclick"]')
+ps = tree.xpath('//div[@id="contenido"]//div[@id="zonaprime"]//p')
 for new, p in zip(news, ps):
     print(' '.join(list(new.itertext())))
     print(' '.join(list(p.itertext())), end='\n\n')
