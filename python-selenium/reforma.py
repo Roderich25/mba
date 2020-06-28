9e07cce88ac7a704359582d33cf98efa5902cbb9
Roderich25 <rodrigoavilasolis@gmail.com>
2020-06-28 07:44:08 -0500

    update

--- python-selenium/reforma.py
+++ python-selenium/reforma.py
@@ -4,8 +4,9 @@ import requests
 page = requests.get('http://www.reforma.com')
 tree = html.fromstring(page.content)
 
-news = tree.xpath('//div[@id="contenido"]//div[@id="zonaprime"]//h1//a[@class="ligaonclick"]')
-ps = tree.xpath('//div[@id="contenido"]//div[@id="zonaprime"]//p')
+main_tag = 'unodostres'  # 'zonaprime'
+news = tree.xpath(f'//div[@id="contenido"]//div[@id="{main_tag}"]//h1//a[@class="ligaonclick"]')
+ps = tree.xpath(f'//div[@id="contenido"]//div[@id="{main_tag}"]//p')
 for new, p in zip(news, ps):
     print(' '.join(list(new.itertext())))
     print(' '.join(list(p.itertext())), end='\n\n')
