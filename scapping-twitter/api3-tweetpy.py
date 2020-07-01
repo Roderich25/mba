fb7c0c9859e936253a0032343fea7f2db53cbcd0
Roderich25 <rodrigoavilasolis@gmail.com>
2020-07-01 17:09:31 -0500

    adding another api

---
+++ scapping-twitter/api3-tweetpy.py
@@ -0,0 +1,13 @@
+from os import getenv
+import tweepy
+
+
+consumer_key = getenv('API_KEY')
+consumer_secret = getenv('API_SECRET_KEY')
+access_token_key = getenv('ACCESS_TOKEN')
+access_token_secret = getenv('ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_secret)
+
+api = tweepy.API(auth)
