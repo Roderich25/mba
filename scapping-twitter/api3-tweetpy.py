9b888295d0df39156e651547bef1009e0238cba4
Roderich25 <rodrigoavilasolis@gmail.com>
2020-07-01 17:13:51 -0500

    update

--- scapping-twitter/api3-tweetpy.py
+++ scapping-twitter/api3-tweetpy.py
@@ -11,3 +11,10 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token_key, access_token_secret)
 
 api = tweepy.API(auth)
+
+user = api.get_user('ConacytYa')
+print(user.screen_name)
+print(user.followers_count)
+for friend in user.friends():
+    print(friend.screen_name)
+
