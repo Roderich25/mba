625975f6fc30a99795d4eaf3c63a38312b29e995
Roderich25 <rodrigoavilasolis@gmail.com>
2020-07-06 21:13:53 -0500

    useReducer demo

--- breaking-bad-react-app/src/App.js
+++ breaking-bad-react-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 import Header from "./components/ui/Header";
 import Search from "./components/ui/Search";
@@ -7,11 +7,32 @@ import "./App.css";
 
 export const MyContext = React.createContext();
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count++ };
+    case "decrement":
+      return { count: state.count-- };
+    default:
+      return state;
+  }
+}
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
   const [items, setItems] = useState([]);
   const [theme, setTheme] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState("");
+
+  const increment = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -33,14 +54,19 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-      <MyContext.Provider value={theme}>
-        <button onClick={toggleTheme}>Toggle</button>
-        <Header />
-        <Search getQuery={(q) => setQuery(q)} />
-      </MyContext.Provider>
-      <CharacterGrid isLoading={isLoading} items={items} />
-    </div>
+    <React.Fragment>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+      <div className="container">
+        <MyContext.Provider value={theme}>
+          <button onClick={toggleTheme}>Toggle</button>
+          <Header />
+          <Search getQuery={(q) => setQuery(q)} />
+        </MyContext.Provider>
+        <CharacterGrid isLoading={isLoading} items={items} />
+      </div>
+    </React.Fragment>
   );
 };
 
