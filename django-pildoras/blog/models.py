cb213ee72cbf459b8a9f946cb74be12df10b9c95
Roderich25 <rodrigoavilasolis@gmail.com>
2020-08-19 10:43:42 -0500

    adding blog app

---
+++ django-pildoras/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        return self.titulo
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.CharField(max_length=150)
+    imagen = models.ImageField(upload_to='servicios')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Servicio'
+        verbose_name_plural = 'Servicios'
+
+    def __str__(self):
+        return self.titulo
