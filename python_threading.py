bd8dbc0d753e86d7d08da55c41b8866851249b16
Roderich25 <rodrigoavilasolis@gmail.com>
2020-08-31 18:13:38 -0500

    simple thread

---
+++ python_threading.py
@@ -0,0 +1,15 @@
+import time
+import threading
+
+
+def namedfunc(name):
+    print(f'\tfunc {name} started.')
+    time.sleep(5)
+    print(f'\tfunc {name} finished.')
+
+
+if __name__ == '__main__':
+    print('\nmain started.')
+    t = threading.Thread(target=namedfunc, args=['test'])
+    t.start()
+    print('\nmain finished.')
