cecdf26b92e180b1572b68f14c26248451e69af9
Roderich25 <rodrigoavilasolis@gmail.com>
2020-09-01 08:45:51 -0500

    many threads

--- python_threading.py
+++ python_threading.py
@@ -1,16 +1,34 @@
-import time
+from time import sleep
 import threading
 
 
-def namedfunc(name):
-    print(f'\tfunc {name} started.')
-    time.sleep(5)
-    print(f'\tfunc {name} finished.')
+def namedfunc1(name):
+    print(f'\n\tfunc1 {name} started.')
+    sleep(5)
+    print(f'\tfunc1 {name} finished.')
+
+
+def namedfunc2(name):
+    print(f'\n\tfunc2 {name} started.')
+    sleep(5)
+    print(f'\n\tfunc2 {name} finished.')
+
+
+def namedfunc3(name):
+    print(f'\n\tfunc3 {name} started.\n')
+    sleep(5)
+    print(f'\n\tfunc3 {name} finished.\n')
 
 
 if __name__ == '__main__':
     print('\nmain started.')
-    t = threading.Thread(target=namedfunc, args=['test'])  # daemon=True
-    t.start()
-    t.join()
+    t1 = threading.Thread(target=namedfunc1, args=['foo'])  # daemon=True
+    t1.start()
+    t2 = threading.Thread(target=namedfunc2, args=['bar'])
+    t2.start()
+    t3 = threading.Thread(target=namedfunc3, args=['baz'])
+    t3.start()
+    t1.join()
+    t2.join()
+    t3.join()
     print('\nmain finished.')
