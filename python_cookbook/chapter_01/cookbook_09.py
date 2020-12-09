24fe5b91a7d8e17a1a5e64ad5c3784aab464149e
Roderich25 <rodrigoavilasolis@gmail.com>
2020-12-09 11:03:44 -0600

    update

--- python_cookbook/chapter_01/cookbook_09.py
+++ python_cookbook/chapter_01/cookbook_09.py
@@ -13,3 +13,16 @@ b = {
     'x': 11,
     'y': 2,
 }
+
+# keys in common
+print(a.keys() & b.keys())
+
+# keys in a but not in b
+print(a.keys() - b.keys())
+
+# key,value pairs in common
+print(a.items() & b.items())
+
+# new dictionary with keys removed
+c = {key: a[key] for key in a.keys()-{'z', 'w'}}
+print(c)
