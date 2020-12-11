184450a8ea4da00d24d4b280bf738a9e577174ba
Roderich25 <rodrigoavilasolis@gmail.com>
2020-12-10 21:43:49 -0600

    update

--- python_cookbook/chapter_01/cookbook_20.py
+++ python_cookbook/chapter_01/cookbook_20.py
@@ -1,3 +1,23 @@
 
 
 # Combining multiple mappings into a single mapping
+from collections import ChainMap
+
+a = {'x': 1, 'z': 3}
+b = {'y': 2, 'z': 4}
+
+c = ChainMap(a, b)
+print(c['x'])
+print(c['y'])
+print(c['z'])
+
+print(len(c))
+print(list(c.keys()))
+print(list(c.values()))
+
+# operations tha mutate mappings always affect the first mapping
+c['z'] = 10
+c['w'] = 40
+del c['x']
+print(a)
+# del c['y'] # KeyError: "Key not found in the first mapping: 'y'"
