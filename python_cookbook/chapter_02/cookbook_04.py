e6316d381a478ecf05868cbd09760e8cf952ec41
Roderich25 <rodrigoavilasolis@gmail.com>
2020-12-12 16:45:13 -0600

    update

---
+++ python_cookbook/chapter_02/cookbook_04.py
@@ -0,0 +1,10 @@
+
+
+# 2.4 Matching and searching for text patterns
+
+text = 'yeah, but no, but yeah, but no, but yeah'
+print(text == 'yeah')
+print(text.startswith('yeah'))
+print(text.endswith('no'))
+print(text.find('no'))
+print(text[10:])
