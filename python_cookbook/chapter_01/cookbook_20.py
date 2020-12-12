ba08801e227ca7a7f278accc09b4c7e3900407f8
Roderich25 <rodrigoavilasolis@gmail.com>
2020-12-11 19:29:32 -0600

    update

--- python_cookbook/chapter_01/cookbook_20.py
+++ python_cookbook/chapter_01/cookbook_20.py
@@ -21,3 +21,41 @@ c['w'] = 40
 del c['x']
 print(a)
 # del c['y'] # KeyError: "Key not found in the first mapping: 'y'"
+
+
+# another example
+values = ChainMap()
+values['x'] = 1
+values = values.new_child()
+values['x'] = 2
+values = values.new_child()
+values['x'] = 3
+print(values)
+print(values['x'])
+values = values.parents
+print(values['x'])
+values = values.parents
+print(values['x'])
+print(values)
+
+
+# another example
+a = {'x': 1, 'z': 3}
+b = {'y': 2, 'z': 4}
+
+merged = dict(b)
+print(merged)
+merged.update(a)
+print(merged)
+
+a['x'] = 123  # dictionary `a` is updated
+print(merged['x'])  # but merged one doesn't
+
+
+# another example
+a = {'x': 1, 'z': 3}
+b = {'y': 2, 'z': 4}
+merged = ChainMap(a, b)
+print(merged['x'])
+a['x'] = 123  # ChainMap is updated
+print(merged['x'])
