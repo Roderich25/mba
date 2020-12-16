8ac52cc81d78e262b9823de5a125031b9e607046
Roderich25 <rodrigoavilasolis@gmail.com>
2020-12-16 10:29:35 -0600

    update

--- python_cookbook/chapter_02/cookbook_05.py
+++ python_cookbook/chapter_02/cookbook_05.py
@@ -9,9 +9,8 @@ text = text.replace('yeah', 'yep')
 print(text)
 
 
-text = 'Today is 11/27/2012. PyCon starts 3/13/2013'
-text = re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', text)
-print(text)
+text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+print(re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', text))
 
 
 # another example | using re compile method
@@ -19,6 +18,16 @@ datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
 print(datepat.sub(r'\3-\2-\1', text))
 
 
-# another example | using mont_abbr function
+# another example | using month_abbr mapping
 def change_date(m):
-    pass
+    mon_name = month_abbr[int(m.group(1))]
+    return f'{m.group(2)} {mon_name} {m.group(3)}'
+
+
+print(datepat.sub(change_date, text))
+
+
+# another example
+newtext, n = datepat.subn(r'\3-\1-\2', text)
+print(newtext)
+print(n)
