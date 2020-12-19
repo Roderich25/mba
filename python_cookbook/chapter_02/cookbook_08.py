dc510879e25ef484db40122d5973d5f6dc7a6805
Roderich25 <rodrigoavilasolis@gmail.com>
2020-12-18 18:24:25 -0600

    update

--- python_cookbook/chapter_02/cookbook_08.py
+++ python_cookbook/chapter_02/cookbook_08.py
@@ -1,3 +1,14 @@
 
 
 # 2.8 Writing a regular expression for multiline patterns
+import re
+
+comment = re.compile(r'/\*(.*?)\*/')
+text1 = '/* this is a comment */'
+text2 = '''
+        /* this is
+        a multiline
+        comment*/
+'''
+print(comment.findall(text1))
+print(comment.findall(text2))
