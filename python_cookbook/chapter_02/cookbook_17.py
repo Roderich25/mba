7aec1a4e3688c97027d7e64692170af7f6c5d8f3
Roderich25 <rodrigoavilasolis@gmail.com>
2021-01-27 21:50:06 -0600

    update

--- python_cookbook/chapter_02/cookbook_17.py
+++ python_cookbook/chapter_02/cookbook_17.py
@@ -2,6 +2,8 @@
 
 # 2.17 Handling HTML and XML entities in text
 import html
+from html.parser import HTMLParser
+from xml.sax.saxutils import unescape
 
 s = 'Elements are written as "<tag>text</tag>".'
 print(s)
@@ -10,3 +12,10 @@ print(html.escape(s))
 
 s = 'Spicy Jalapeño'
 print(s.encode('ascii', errors='xmlcharrefreplace'))
+
+s = "Spicy &quot;Jalape&#241;o&quot."
+p = HTMLParser()
+print(p.unescape(s))
+
+t = 'The prompt is &gt;&gt;&gt;'
+print(unescape(t))
