5f02e4a9d8dd6457772a0a3893173a07b2db74f4
Roderich25 <rodrigoavilasolis@gmail.com>
2021-03-11 19:08:54 -0600

    updating class based views for blog app

--- django-pildoras/blog/urls.py
+++ django-pildoras/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogView
+from .views import BlogView, CategoryView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog'),
+    path('categoria/<int:category_id>/', CategoryView.as_view(), name='categoria'),
 ]
--- django-pildoras/blog/views.py
+++ django-pildoras/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from blog.models import Post, Categoria
+from django.shortcuts import get_object_or_404
 
 
 class BlogView(ListView):
@@ -7,3 +8,20 @@ class BlogView(ListView):
     context_object_name = 'posts'
     extra_context = {'categorias': Categoria.objects.all()}
     template_name = 'blog/blog.html'
+
+
+class CategoryView(ListView):
+    # pk_url_kwarg = 'category_id'
+    template_name = 'blog/categorias.html'
+    context_object_name = 'categories'
+    queryset = Post.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(self.__class__, self).get_context_data(**kwargs)
+        cat = get_object_or_404(Categoria, pk=self.kwargs.get('category_id'))
+        context['category'] = cat.nombre
+        return context
+
+    def get_queryset(self):
+        cid = self.kwargs.get('category_id')
+        return self.queryset.filter(categorias__id=cid)
