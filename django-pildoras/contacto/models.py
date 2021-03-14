2bfadb3557e09dac9e1f97d0fd19fc5291b1599d
Roderich25 <rodrigoavilasolis@gmail.com>
2021-03-14 17:15:16 -0600

    form with django messages

--- django-pildoras/contacto/models.py
+++ django-pildoras/contacto/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Contacto(models.Model):
+    pass
--- django-pildoras/contacto/views.py
+++ django-pildoras/contacto/views.py
@@ -1,9 +1,18 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views.generic import FormView
 from .forms import ContactoForm
+from django.contrib import messages
 
 
 class ContactoView(FormView):
     template_name = 'contacto/contacto.html'
     form_class = ContactoForm
-    # initial = {'nombre': 'tu nombre', 'email': 'tu correo', 'contenido': 'tu mensaje'}
-    success_url = "/contacto/"
+    success_url = reverse_lazy('contacto:contacto')
+
+    def form_valid(self, form):
+        print("FORM_VALID", self.request.POST)
+        nombre = self.request.POST.get('nombre', 'Usuario')
+        correo = self.request.POST.get('email', None)
+        messages.success(self.request, f'Gracias {nombre}, en breve obtendra una respuesta a su email: {correo}.')
+        return HttpResponseRedirect(self.get_success_url())
