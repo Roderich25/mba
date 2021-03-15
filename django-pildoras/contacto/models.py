9eb82625123c82243923b08746d7285e06979312
Roderich25 <rodrigoavilasolis@gmail.com>
2021-03-14 19:20:18 -0600

    starting app tienda

--- django-pildoras/contacto/models.py
+++ django-pildoras/contacto/models.py
@@ -1,5 +1,8 @@
-from django.db import models
+from django.db.models import Model, CharField, TextField, DateTimeField
 
 
-class Contacto(models.Model):
-    pass
+class Contacto(Model):
+    nombre = CharField(max_length=128)
+    email = CharField(max_length=128)
+    contenido = TextField()
+    fecha = DateTimeField()
--- django-pildoras/contacto/views.py
+++ django-pildoras/contacto/views.py
@@ -5,6 +5,7 @@ from .forms import ContactoForm
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.utils import timezone
+from contacto.models import Contacto
 
 
 class ContactoView(FormView):
@@ -16,11 +17,15 @@ class ContactoView(FormView):
         nombre = self.request.POST.get('nombre', 'Usuario')
         correo = self.request.POST.get('email', None)
         contenido = self.request.POST.get('contenido', None)
-        dte = timezone.now().strftime('%B/%d/%Y')
+        dte = timezone.now()
+
+        c = Contacto(nombre=nombre, email=correo, contenido=contenido, fecha=dte)
+        c.save()
+
         messages.success(self.request, f'Gracias {nombre}, en breve obtendra una respuesta a su email: {correo}.')
 
         send_mail(subject="Thanks for contacting us",
-                  message=f"You contacted us on {dte} about {contenido}.\nYou'll receive an answer soon.",
+                  message=f"You contacted us on {dte.strftime('%B/%d/%Y')} about {contenido}.\nYou'll receive an answer soon.",
                   from_email="rodrigo@contacto.com",
                   recipient_list=[correo],
                   fail_silently=False)
