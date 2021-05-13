25b2267cec33ac1d3a711c42166a12f4716a3ff8
Roderich25 <rodrigoavilasolis@gmail.com>
2021-05-13 13:51:01 -0500

    update

--- django-pildoras/carro/views.py
+++ django-pildoras/carro/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from tienda.models import Producto
+from .carro import Carro
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def agregar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.agregar(producto)
+    return redirect('tienda:tienda')
+
+
+def eliminar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.eliminar(producto)
+    return redirect('tienda:tienda')
+
+
+def restar_producto(request, producto_id):
+    carro = Carro(request)
+    producto = Producto.objects.get(id=producto_id)
+    carro.restar(producto)
+    return redirect('tienda:tienda')
+
+
+def limpiar_carro(request):
+    carro = Carro(request)
+    carro.vaciar()
+    return redirect('tienda:tienda')
